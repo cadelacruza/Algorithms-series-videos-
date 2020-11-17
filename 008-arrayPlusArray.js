fbaf7c7a91c6d5a82136754949f0c90a3d971781
cadelacruza <carlosdelacruzacosta4@gmail.com>
2020-11-16 22:55:55 -0600

    added three  problems

---
+++ 008-arrayPlusArray.js
@@ -0,0 +1,11 @@
+function arrayPlusArray(arr1, arr2) {
+    const sumOne = arra1.reduce((accumulator, current) => accumulator + current);
+    const sumTwo = arra2.reduce((accumulator, current) => accumulator + current);
+
+    return sumOne + sumTwo;
+  }
+
+
+  //[1,2,3] 
+  // 1
+  // 2
